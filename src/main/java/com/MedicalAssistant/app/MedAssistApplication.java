6db9b05e61ce2de348f09a9e74eb810ef4ca3225
stanko <risimicstanko@gmail.com>
2023-11-21 09:43:11 +0100

    start od an application

---
+++ src/main/java/com/MedicalAssistant/app/MedAssistApplication.java
@@ -0,0 +1,14 @@
+package com.MedicalAssistant.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedAssistApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MedAssistApplication.class, args);
+	}
+
+}
